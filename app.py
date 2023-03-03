00645de3229a3df255643dd54e941c05697115db
Ayush More <ayushmore@Ayushs-MacBook-Air.local>
2023-03-03 12:06:41 -0800

    Fixed the refreshing of the page and kept the user inputs in the text fields. Also created session variables in Flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import lyricsgenius as lg
 import openai
 import os
@@ -11,22 +11,30 @@ genius = lg.Genius(os.getenv('LYRICS_GENIUS_API_KEY'), timeout=10)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
+app.secret_key = os.getenv('APP_SECRET_KEY')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        print("In the index POST!")
         title = request.form['title']
+        session['title'] = title
         artist = request.form['artist']
+        session['artist'] = artist
         length = request.form['length']
-        return redirect(url_for('result', title = title, artist = artist, length = length))
+        session['length'] = length
+        return redirect(url_for('result'))
     
-    return render_template('home.html', title='', artist='', length='', summary=None)
+    session.clear()
+    return render_template('index.html', title='', artist='', length='', summary=None)
 
 @app.route('/result')
 def result():
-    title = request.args.get('title')
-    artist = request.args.get('artist')
-    length = request.args.get('length')
+    if not session.get('title'):
+        return redirect(url_for('index'))
+    title = session.get('title')
+    artist = session.get('artist')
+    length = session.get('length')
     try:
         song = genius.search_song(title, artist)
         context = "You are a renowned expert in understanding and explaining the lyrics of popular songs. A major music magazine has asked you to provide an in-depth analysis of the lyrics of a new hit song that has taken the music world by storm. Your job is to break down the meaning of the song and explain the underlying themes and metaphors used by the songwriter. You must provide a detailed and insightful analysis that will leave the magazine's readers with a greater appreciation and understanding of the song."
@@ -39,6 +47,7 @@ def result():
                 ]
         )
         summary = response["choices"][0]["message"]["content"]
+        session.clear()
         return render_template('index.html', summary=summary, title=title, artist=artist)
     except Exception as e:
         error = "Song does not exist! Check your spelling and try again."
