a9660e99440e7a7fa49780554aefb84b1a9de7ec
Ayush More <ayushmore@Ayushs-MacBook-Air.local>
2023-03-16 21:09:46 -0700

    Removed length button to generate one sentence summary instead

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def index():
     if request.method == 'POST':
         session['title'] = request.form['title']
         session['artist'] = request.form['artist']
-        session['length'] = request.form['length']
         return redirect(url_for('result'))
     
     session.clear()
@@ -33,15 +32,15 @@ def result():
         return redirect(url_for('index'))
     title = session.get('title')
     artist = session.get('artist')
-    length = session.get('length')
+    # length = session.get('length')
     try:
         start_time = time.time()
         song = genius.search_song(title, artist)
         end_time = time.time()
 
         print(f"Time taken to search song: {end_time - start_time} seconds")
-        context = "You are a renowned expert in understanding and explaining the lyrics of popular songs. A major music magazine has asked you to provide an in-depth analysis of the lyrics of a new hit song that has taken the music world by storm. Your job is to break down the meaning of the song and explain the underlying themes and metaphors used by the songwriter. You must provide a detailed and insightful analysis that will leave the magazine's readers with a greater appreciation and understanding of the song."
-        prompt_refined = f"I'd like you to summarize the meaning of the lyrics of a song that I'll provide. Your task is to listen to the song and identify the key themes and messages conveyed by the lyrics. You should then provide {length} summarizing the lyrics in a way that captures the essence of the song and the emotions it conveys. Your summary should be concise yet insightful, and should provide a clear understanding of the song's meaning to someone who may not be familiar with it. The song is {song.title} by {song.artist}. The lyrics are provided below in a JSON format:\n\n{{\"Lyrics\":  {song.lyrics}}}"
+        context = f"You are a master wordsmith and musical connoisseur with an engaging and humorous tone tasked with providing a thought-provoking and original 'lyrical insight' into the hit song '{title}' by '{artist}'"
+        prompt_refined = f"Your mission is to provide a 'lyrical insight' into the song that captures its essence. Your lyrical insight should be: a single, powerful, in-depth sentence that captures the essence of the song and its underlying themes. Provide a unique and captivating 'lyrical insight' that sheds new light on the song's message that amazes the readers. Be engaging and humorous in your tone. The lyrics are provided below in a JSON format:\n\n{{\"Lyrics\":  {song.lyrics}}}"
         start_time = time.time()
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
